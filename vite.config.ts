5f30394af5fce337f5bbf5ce5fb8255ac7d5f3a9
wensheng <948290581@qq.com>
2021-11-29 10:48:07 +0800

    Update vite.config.ts
    To create a demo page

--- vite.config.ts
+++ vite.config.ts
@@ -6,35 +6,102 @@ const src_root_dir = join(__dirname, 'src');
 const dist_root_dir = join(__dirname, 'dist');
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    root: src_root_dir,
-    resolve: {
-        alias: {
-            '@': src_root_dir,
-        },
-    },
-    base: './',
-    build: {
-        outDir: dist_root_dir,
-        minify: true,
-        sourcemap: false,
-        brotliSize: true,
-        emptyOutDir: true,
-        lib: {
-            entry: join(src_root_dir, 'main.ts'),
-            formats: ['umd', 'es'],
-            name: 'tools',
-        },
-    },
-    server: {
-        port: 3021,
-    },
-    css: {
-        preprocessorOptions: {
-            less: {
-                javascriptEnabled: true,
+export default defineConfig(({command, mode}) => {
+    if (mode === 'demo') {
+        return {
+            root: src_root_dir,
+            resolve: {
+                alias: {
+                    '@': src_root_dir,
+                },
             },
-        },
-    },
-    plugins: [vue()],
+            base: './',
+            build: {
+                outDir: dist_root_dir,
+                minify: true,
+                sourcemap: false,
+                brotliSize: true,
+                emptyOutDir: true,
+            },
+            server: {
+                port: 3021,
+            },
+            css: {
+                preprocessorOptions: {
+                    less: {
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+            plugins: [vue()],
+        };
+    }
+    if (command === 'serve') {
+        return {
+            root: src_root_dir,
+            resolve: {
+                alias: {
+                    '@': src_root_dir,
+                },
+            },
+            base: './',
+            build: {
+                outDir: dist_root_dir,
+                minify: true,
+                sourcemap: false,
+                brotliSize: true,
+                emptyOutDir: true,
+                lib: {
+                    entry: join(src_root_dir, 'main.ts'),
+                    formats: ['umd', 'es'],
+                    name: 'tools',
+                },
+            },
+            server: {
+                port: 3021,
+            },
+            css: {
+                preprocessorOptions: {
+                    less: {
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+            plugins: [vue()],
+        };
+    } else {
+        // command === 'build'
+        return {
+            root: src_root_dir,
+            resolve: {
+                alias: {
+                    '@': src_root_dir,
+                },
+            },
+            base: './',
+            build: {
+                outDir: dist_root_dir,
+                minify: true,
+                sourcemap: false,
+                brotliSize: true,
+                emptyOutDir: true,
+                lib: {
+                    entry: join(src_root_dir, 'main.ts'),
+                    formats: ['umd', 'es'],
+                    name: 'tools',
+                },
+            },
+            server: {
+                port: 3021,
+            },
+            css: {
+                preprocessorOptions: {
+                    less: {
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+            plugins: [vue()],
+        };
+    }
 });
